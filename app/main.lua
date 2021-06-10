f6c7f3753e43a68c4233ea7b703d128c06100f8c
涵曦 <im.hanxi@gmail.com>
2021-06-10 06:34:40 +0000

    add 404 and test

--- app/main.lua
+++ app/main.lua
@@ -1,13 +1,25 @@
 local wlua = require "wlua"
 local log = require "log"
 local app = wlua:new()
+local util_json = require "util.json"
 
 app:get("/", function (c)
     c:send("Hello wlua!")
 end)
 
 app:get("/foo", function (c)
+    log.debug("c.req.query:", util_json.encode(c.req.query))
     c:send("bar")
+end)
+
+app:post("/foo/", function (c)
+    log.debug("c.req.query:", util_json.encode(c.req.query))
+    log.debug("c.req.body:", util_json.encode(c.req.body))
+    c:send("bar/")
+end)
+
+app:set_no_route(function(c)
+    c:send("Not found", 404)
 end)
 
 app:run()
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+curl -i http://127.0.0.1:8081/foo?a=b
+
+curl -i http://127.0.0.1:8081/foobar
+
+curl -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' -i http://127.0.0.1:8081/foo/?b=c
+
